6a2df5ce40d87f994257cdf50a296409b467df25
@JonatasAriel <jonatas.cruz@jala.university>
2024-11-19 17:23:00 -0300

    Account module integration

---
+++ src/main/java/org/jala/university/application/service/AuthenticationServiceImpl.java
@@ -0,0 +1,61 @@
+package org.jala.university.application.service;
+
+import org.jala.university.domain.entity.Authentication;
+import org.jala.university.domain.repository.AuthenticationRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final AuthenticationRepository<Authentication, Integer> authenticationRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationServiceImpl(AuthenticationRepository<Authentication, Integer> authenticationRepository) {
+        this.authenticationRepository = authenticationRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public Authentication register(Authentication authentication) {
+        return authenticationRepository.save(authentication);
+    }
+
+    @Override
+    public Optional<Authentication> getAuthentication(Integer id) {
+        return authenticationRepository.findById(id);
+    }
+//
+//    @Override
+//    public Optional<Authentication> getAuthenticationByEmail(String email) {
+//        return authenticationRepository.findByEmail(email);
+//    }
+
+    @Override
+    public void deleteAuthentication(Integer id) {
+        authenticationRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean authenticate(String cpf, String password) {
+        Optional<Authentication> authOpt = authenticationRepository.findByCpf(cpf);
+        if (authOpt.isPresent()) {
+            Authentication auth = authOpt.get();
+            // Usando o PasswordEncoder para verificar a senha
+            return passwordEncoder.matches(password, auth.getPassword());
+        }
+        return false;
+    }
+
+    @Override
+    public void resetPassword(String email) {
+        // Implementação da lógica de redefinição de senha, se necessário.
+    }
+
+    @Override
+    public void register(Integer id, String text) {
+        // Implementação para o método register com id, se necessário.
+    }
+}
---
+++ src/main/java/org/jala/university/domain/entity/Account.java
@@ -0,0 +1,41 @@
+package org.jala.university.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String accountNumber;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+
+    private BigDecimal balance;
+
+    private Currency currency;
+
+    @Column(nullable = false) // Define o campo como obrigatório
+    private Integer customerId; // Adiciona o campo customerId
+
+    @Column(name = "payment_password")
+    private String paymentPassword;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+}
