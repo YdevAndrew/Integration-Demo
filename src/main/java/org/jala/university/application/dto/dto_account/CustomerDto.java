bbe4c31f4cfee8e4801d2e3d513664f9312389c5
Rafael Macedo <rafael.macedo@jala.university>
2024-11-23 19:08:01 -0300

    customerDTO updated

--- src/main/java/org/jala/university/application/dto/dto_account/CustomerDto.java
+++ src/main/java/org/jala/university/application/dto/dto_account/CustomerDto.java
@@ -1,17 +1,29 @@
 package org.jala.university.application.dto.dto_account;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Value;
+import lombok.*;
 
+import java.time.LocalDate;
 
-import java.util.UUID;
-
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@Value
-public class CustomerDTO {
-    Integer id;
-    String fullName;
-    String cpf;
+public class CustomerDto {
+
+    private Integer id;
+    private String fullName;
+    private String cpf;
+    private String email;
+    private String gender;
+    private String phoneNumber;
+    private LocalDate birthday;
+    private String country;
+    private String district;
+    private String state;
+    private String postalCode;
+    private String street;
+    private byte[] password;
+    private String verificationCode;
+
 }
