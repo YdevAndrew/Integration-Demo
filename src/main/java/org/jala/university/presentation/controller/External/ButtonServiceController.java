671f74fba701c1a1e6250b2285bacbce13fb0e0c
kacterine <kacterine.martinez@jala.university>
2024-11-25 21:13:41 -0400

    Alteração dos dados da lista de serviços

--- src/main/java/org/jala/university/presentation/controller/External/ButtonServiceController.java
+++ src/main/java/org/jala/university/presentation/controller/External/ButtonServiceController.java
@@ -7,43 +7,35 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import org.jala.university.commons.presentation.BaseController;
 import org.springframework.stereotype.Controller;
+import javafx.scene.control.Button;
 
+
+import java.awt.*;
 import java.io.IOException;
 
 @Controller
 public class ButtonServiceController extends BaseController {
+
+    private String serviceName;
+
+    @FXML
+    private Button serviceNameButton;
+
     @FXML
     private AnchorPane mainContent; // Altere para AnchorPane para corresponder ao tipo em ButtonService.fxml
 
+    // Método para definir os dados do pagamento com validações
+    public void initialize(String serviceName) {
+
+        this.serviceName = serviceName;
+
+        serviceNameButton.setText("serviceName");
+    }
+
     @FXML
-    public void handleService1Click(ActionEvent event) throws IOException {
+    public void handleServiceClick(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/External/ScheduleServices/ServiceDetail.fxml"));
         Pane serviceDetailContent = loader.load();
         mainContent.getChildren().setAll(serviceDetailContent);
-    }
-
-    @FXML
-    public void handleService2Click(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/External/ScheduleServices/ServiceDetail.fxml"));
-        Pane serviceDetailContent = loader.load();
-        mainContent.getChildren().setAll(serviceDetailContent);
-    }
-
-    @FXML
-    public void handleService3Click(ActionEvent event) {
-        System.out.println("Service 3 clicked");
-        // Adicione a ação desejada aqui
-    }
-
-    @FXML
-    public void handleService4Click(ActionEvent event) {
-        System.out.println("Service 4 clicked");
-        // Adicione a ação desejada aqui
-    }
-
-    @FXML
-    public void handleService5Click(ActionEvent event) {
-        System.out.println("Service 5 clicked");
-        // Adicione a ação desejada aqui
     }
 }
